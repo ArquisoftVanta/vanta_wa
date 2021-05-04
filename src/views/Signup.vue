76516f2f4b06063ce617d60caa9ba458060a4cd4
Francisco Dueñas <frsduenasca@unal.edu.co>
2021-05-03 19:28:38 -0500

    :sparkles: Signup view was modified

--- src/views/Signup.vue
+++ src/views/Signup.vue
@@ -1,172 +1,157 @@
 <template>
   <div>
-    <div class="fixed-top">
-      <nav id="Banner" class="navbar navbar-expand-lg">
-        <div class="col-auto">
-          <a id="Titulo" class="navbar-brand text-white" href="#">
-            <img
-              src="~@/assets/logo.png"
-              width="40"
-              height="40"
-              alt=""
-              loading="lazy"
-            />
-            Vanta
-          </a>
-        </div>
-        <button
-          class="navbar-toggler navbar-light bg-light"
-          type="button"
-          data-toggle="collapse"
-          data-target="#navbarSupportedContent"
-          aria-controls="navbarSupportedContent"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
-          <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul class="navbar-nav mr-auto"></ul>
-          <ul class="navbar-nav mr-right">
-            <li class="nav-item active">
-              <router-link to="about-us" class="nav-link text-white"
-                >Acerca de nosotros</router-link
-              >
-            </li>
-            <li class="nav-item">
-              <router-link to="/login" class="nav-link text-white"
-                >Ingresar</router-link
-              >
-            </li>
-          </ul>
-        </div>
-      </nav>
-    </div>
-    <div class="container-fluid mb-5">
+    <Header></Header>
+    <div class="container-fluid mb-3">
       <div class="row">
-        <div class="col-12 col-lg-4 offset-lg-4 mt-5">
-          <div class="card mb-5">
+        <div class="col-12 col-lg-4 offset-lg-4">
+          <div class="card shadow">
             <div class="form-col text-dark align-items-center">
               <div class="card-body">
-                <img class="logo mb-4" alt="logo" src="~@/assets/logo.png" />
-                <h5>Ingresa tus datos</h5>
-                <div class="col-auto">
-                  <div class="form-group">
-                    <label class="text-dark text-left" for="name">Nombre</label>
-                    <input
-                      v-model="newUser.userName"
-                      type="text"
-                      class="form-control"
-                      placeholder="Nombre completo"
-                      name="name"
-                      id="inputName"
-                      required
-                    />
-                  </div>
-                </div>
-                <div class="col-auto">
-                  <div class="form-group">
-                    <label class="text-dark" for="phone"></label>
-                    <input
-                      v-model="newUser.userPhone"
-                      type="tel"
-                      class="form-control"
-                      placeholder="Telefono"
-                      name="phone"
-                      pattern="[0-9]{10}"
-                      id="inputPhone"
-                      required
-                    />
-                  </div>
-                </div>
-                <div class="col-auto">
-                  <div class="form-row">
-                    <div class="col">
-                      <input
-                        v-model="email"
-                        type="text"
-                        class="form-control"
-                        placeholder="Usuario"
-                        name="user"
-                        id="inputUser"
-                        required
-                      />
-                    </div>
-                    <div class="col">
-                      <label for="staticEmail"> @unal.edu.co </label>
+                <!-- <img class="logo mb-4" alt="logo" src="~@/assets/logo.png" /> -->
+                <h5 class="text-muted">Únete a vanta</h5>
+                <h2 class="mb-4 font-weight-bold">Crea tu cuenta</h2>
+                <form @submit="signup">
+                  <div class="col-auto">
+                    <div class="form-group">
+                      <div class="input-group input-group-sm mb-3">
+                        <div class="input-group-prepend">
+                          <span class="input-group-text" id="inputGroup-sizing-sm"
+                            >Nombre</span
+                          >
+                        </div>
+                        <input
+                          v-model="newUser.userName"
+                          type="text"
+                          class="form-control"
+                          placeholder="Nombre completo"
+                          aria-label="Sizing example input"
+                          aria-describedby="inputGroup-sizing-sm"
+                          required
+                        />
+                      </div>
                     </div>
                   </div>
-                </div>
-                <div class="col-auto">
-                  <div class="form-row">
-                    <div class="col">
-                      <label for="psw"></label>
-                      <input
-                        v-model="newUser.password"
-                        type="password"
-                        class="form-control"
-                        placeholder="Contraseña"
-                        name="psw"
-                        id="inputPsw"
-                        required
-                      />
+                  <div class="col-auto">
+                    <div class="form-group">
+                      <div class="input-group input-group-sm mb-3">
+                        <input
+                          v-model="email"
+                          type="text"
+                          name="user"
+                          class="form-control"
+                          placeholder="Usuario institucional"
+                          aria-label="Sizing example input"
+                          aria-describedby="inputGroup-sizing-sm"
+                          required
+                        />
+                        <div class="input-group-append">
+                          <span class="input-group-text">@unal.edu.co</span>
+                        </div>
+                      </div>
                     </div>
                   </div>
-                </div>
-                <div class="col-auto">
-                  <div class="form-row">
-                    <div class="col">
-                      <label for="pswRepeat"></label>
-                      <input
-                        v-model="password"
-                        type="password"
-                        class="form-control"
-                        placeholder="Confirmar contraseña"
-                        name="pswRepeat"
-                        id="inputpswRepeat"
-                        required
-                      />
+                  <div class="col-auto">
+                    <div class="form-group">
+                      <div class="input-group input-group-sm mb-3">
+                        <div class="input-group-prepend">
+                          <span class="input-group-text" id="inputGroup-sizing-sm"
+                            >Teléfono/Celular</span
+                          >
+                        </div>
+                        <input
+                          v-model="newUser.userPhone"
+                          type="phone"
+                          class="form-control"
+                          placeholder="Número de teléfono o celular"
+                          pattern="[0-9]{10}"
+                          aria-label="Sizing example input"
+                          aria-describedby="inputGroup-sizing-sm"
+                          required
+                        />
+                      </div>
                     </div>
                   </div>
-                </div>
-                <p>
-                  Al crear una cuenta estas de acuerdo con nuestros
-                  <a href="#" style="color: #06416d">Terminos y Condiciones</a>.
-                </p>
 
-                <button
-                  class="btn btn-dark btn-block"
-                  id="submitBtn"
-                  @click="signup"
-                >
-                  Registrarse
-                </button>
-                <a
-                  href="#"
-                  type="button"
-                  class="btn btn-danger btn-block"
-                  id="cancelBtn"
-                  @click="goToLogin"
-                >
-                  Cancelar
-                </a>
+                  <div class="col-auto">
+                    <div class="form-group">
+                      <div class="input-group input-group-sm mb-3">
+                        <div class="input-group-prepend">
+                          <span class="input-group-text" id="inputGroup-sizing-sm"
+                            >Contraseña</span
+                          >
+                        </div>
+                        <input
+                          v-model="password"
+                          type="password"
+                          class="form-control"
+                          placeholder="Contraseña"
+                          aria-label="Sizing example input"
+                          aria-describedby="inputGroup-sizing-sm"
+                          required
+                        />
+                      </div>
+                    </div>
+                  </div>
+                  <div class="col-auto">
+                    <div class="form-group">
+                      <div class="input-group input-group-sm mb-3">
+                        <div class="input-group-prepend">
+                          <span class="input-group-text" id="inputGroup-sizing-sm"
+                            >Confirmar contraseña</span
+                          >
+                        </div>
+                        <input
+                          v-model="newUser.password"
+                          type="password"
+                          class="form-control"
+                          placeholder="Repite la contraseña"
+                          aria-label="Sizing example input"
+                          aria-describedby="inputGroup-sizing-sm"
+                          required
+                        />
+                      </div>
+                    </div>
+                  </div>
+                  <div class="col-auto">
+                    <p>
+                      <small>Al crear una cuenta estas de acuerdo con nuestros
+                      <a href="about-us">Terminos y Condiciones</a
+                      >.</small>
+                    </p>
+                  </div>
+                  <button
+                    type="submit"
+                    class="btn btn-dark btn-block btn-sm"
+                  >
+                    Registrarse
+                  </button>
+                  <button
+                    href="#"
+                    type="button"
+                    class="btn btn-danger btn-block btn-sm"
+                    id="cancelBtn"
+                    @click="goToLogin"
+                  >
+                    Cancelar
+                  </button>
+                </form>
               </div>
             </div>
           </div>
         </div>
       </div>
     </div>
-    <FooterwithBackground></FooterwithBackground>
   </div>
 </template>
 
 <script>
-import FooterwithBackground from "../components/FooterwithBackground.vue";
 import AuthServiceClient from "../serviceClients/AuthServiceClient";
+import Header from "../components/Header";
+
 export default {
   name: "Signup",
   components: {
-    FooterwithBackground,
+    Header,
   },
 
   data() {
@@ -223,7 +208,7 @@ export default {
         });
       } else {
         AuthServiceClient.registerUser(this.newUser, (response) => {
-          let name = response
+          let name = response;
           if (name.userMail === this.newUser.userMail) {
             this.$router.push("/");
           } else {
