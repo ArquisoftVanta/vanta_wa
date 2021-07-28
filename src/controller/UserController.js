cb6076aa4e1c7866bbaf9f20403d0f49192f64e8
Francisco Dueñas <frsduenasca@unal.edu.co>
2021-07-28 11:22:54 -0500

    :fire: Remove code useless

--- src/controller/UserController.js
+++ src/controller/UserController.js
@@ -2,30 +2,22 @@ import AuthSC from "../serviceClients/AuthServiceClient";
 import UserSC from "../serviceClients/UserServiceClient";
 
 function getUser(callback) {
-    AuthSC.getUserMailByToken((response) => {
-        UserSC.getUser(response, (data) => {
-            /*if (!this.$store.state.user) {
-                this.$store.commit("updateUser", data);
-            }*/
-            callback(data)
-        });
+  AuthSC.getUserMailByToken((response) => {
+    UserSC.getUser(response, (data) => {
+      callback(data);
     });
+  });
 }
 
 function updateUser(user, callback) {
-    AuthSC.getUserMailByToken((response) => {
-        UserSC.updateUser(user, (data) => {
-            callback(data)
-        })
+  AuthSC.getUserMailByToken(() => {
+    UserSC.updateUser(user, (data) => {
+      callback(data);
     });
-
+  });
 }
-
-
-
 
 export default {
-    getUser,
-    updateUser
-
-}
+  getUser,
+  updateUser,
+};
