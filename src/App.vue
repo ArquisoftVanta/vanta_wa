130d16b6a18b17559763b6c1d80136b99ffa5a5c
Francisco Dueñas <frsduenasca@unal.edu.co>
2021-07-30 16:21:24 -0500

    :zap: Change margin-top to zero

--- src/App.vue
+++ src/App.vue
@@ -16,7 +16,7 @@ export default {
   font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  margin-top: 12vh;
+  margin-top: 0vh;
   margin-right: 0vh;
   margin-bottom: 5vh;
   margin-left: 0vh;
