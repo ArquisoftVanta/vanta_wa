d65299836390d623daf96912d8a13a0e1b6412ad
Francisco Dueñas <frsduenasca@unal.edu.co>
2021-07-30 16:24:34 -0500

    :lipstick: New design to Driver.vue

--- src/views/Driver.vue
+++ src/views/Driver.vue
@@ -2,40 +2,34 @@
   <div id="driver">
     <Header></Header>
     <div class="container-fluid mb-5">
-      <div class="row">
-        <div class="col-12 col-md-6 mb-5 mb-md-0">
+      <div class="row justify-content-lg-around">
+        <div class="col-12 col-lg-5 mb-3 mb-lg-0 mr-0 ml-0">
           <div
-            class="card bg-light text-white shadow rounded border border-dark text-center text-dark mr-0 ml-0 mr-sm-5 ml-sm-5"
+            class="card bg-dark shadow text-left text-md-center mr-0 ml-0"
             type="button"
             @click="goToCreateService"
           >
             <img
               class="card-img-top"
-              src="~@/assets/new1.jpg"
+              src="~@/assets/driver.jpg"
               alt="new-service"
             />
             <div class="card-body">
-              <h3 class="card-title d-none d-md-block font-weight-bold">
+              <h1 class="card-title text-warning text-left text-md-center lead">
                 Nuevo servicio
-              </h3>
-              <h3
-                class="card-title d-block d-md-none mb-0 font-weight-bold text-left"
-              >
-                Nuevo servicio
-              </h3>
-              <p class="card-text d-none d-md-block mb-0">
-                <small>
-                  Crea un
-                  <strong>nuevo servicio</strong> que estará a disposición de
-                  todos los <strong>pasajeros</strong>.</small
-                >
+              </h1>
+              <p class="card-text text-light mb-2">
+                Crea un
+                <strong class="text-warning">nuevo servicio</strong>
+                 y seleeciona a tus
+                <strong class="text-warning">pasajeros</strong>.
               </p>
             </div>
           </div>
         </div>
-        <div class="col-12 col-md-6 mb-5 mb-md-0">
+        <div class="col-12 col-lg-5 mb-3 mb-lg-0 mr-0 ml-0">
           <div
-            class="card bg-light text-white shadow rounded border border-dark text-center text-dark mr-0 ml-0 mr-sm-5 ml-sm-5"
+            class="card bg-dark shadow text-left text-md-center mr-0 ml-0"
             type="button"
             @click="goToMyServices"
           >
@@ -45,20 +39,13 @@
               alt="actual-services"
             />
             <div class="card-body">
-              <h3 class="card-title d-none d-md-block font-weight-bold">
-                Servicios actuales
-              </h3>
-              <h3
-                class="card-title d-block d-md-none mb-0 font-weight-bold text-left"
-              >
-                Servicios actuales
-              </h3>
-              <p class="card-text d-none d-md-block mb-0">
-                <small>
-                  Observa todos los <strong>servicios</strong> que tienes
-                  <strong>disponibles</strong> a los
-                  <strong>pasajeros</strong>.</small
-                >
+              <h1 class="card-title text-warning text-left text-md-center lead">
+                Historial de servicios
+              </h1>
+              <p class="card-text text-light mb-2">
+                Mira tu historial de
+                <strong class="text-warning">servicios disponibles</strong>
+                y realizados.
               </p>
             </div>
           </div>
