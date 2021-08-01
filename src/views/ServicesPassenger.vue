be3b0fc9d0b4b061d54f36ce6cc76c2fc776b5c1
Francisco Dueñas <frsduenasca@unal.edu.co>
2021-07-31 22:22:39 -0500

    :zap: New design to ServicesPassenger.vue

--- src/views/ServicesPassenger.vue
+++ src/views/ServicesPassenger.vue
@@ -3,14 +3,14 @@
     <Header></Header>
     <div class="container-fluid mb-5">
       <div class="row">
-        <div class="col-12 col-md-4 offset-md-1">
-          <div class="card mt-4 mb-3 mb-md-4" style="height: 400px">
-            <div class="card">
-              <nav>
-                <div class="nav nav-tabs" id="nav-tab" role="tablist">
+        <div class="col-12 col-lg-6 mb-3 mb-lg-0">
+          <div class="card bg-dark text-center" style="height: 400px">
+            <div class="card-header">
+              <ul class="nav nav-tabs card-header-tabs">
+                <li class="nav-item">
                   <a
-                    class="nav-link"
                     id="active-tab"
+                    class="nav-link btn btn-dark text-light"
                     data-toggle="tab"
                     href="#active"
                     role="tab"
@@ -18,8 +18,10 @@
                     aria-selected="false"
                     >Activos</a
                   >
+                </li>
+                <li class="nav-item">
                   <a
-                    class="nav-link"
+                    class="nav-link btn btn-dark text-light"
                     id="profile-tab"
                     data-toggle="tab"
                     href="#profile"
@@ -28,8 +30,10 @@
                     aria-selected="false"
                     >Seleccionados</a
                   >
+                </li>
+                <li class="nav-item">
                   <a
-                    class="nav-link"
+                    class="nav-link btn btn-dark text-light"
                     id="contact-tab"
                     data-toggle="tab"
                     href="#contact"
@@ -38,226 +42,150 @@
                     aria-selected="false"
                     >Realizados</a
                   >
-                </div>
-              </nav>
+                </li>
+              </ul>
             </div>
-            <div
-              class="card"
-              style="height: 80%; overflow: scroll; margin: 0 0 -20% 0"
-            >
-              <div class="card-body">
-                <div class="tab-content" id="nav-tabContent">
-                  <div
-                    class="tab-pane fade active show"
-                    id="active"
-                    role="tabpanel"
-                    aria-labelledby="active-tab"
-                  >
-                    <div class="accordion" id="accordionExample">
-                      <div
-                        class="card"
+            <div class="card-body overflow-auto">
+              <div class="tab-content" id="nav-tabContent">
+                <div
+                  class="tab-pane fade active show"
+                  id="active"
+                  role="tabpanel"
+                  aria-labelledby="active-tab"
+                >
+                  <table class="table table-hover table-dark table-sm text-nowrap h-100">
+                    <thead>
+                      <tr class="text-warning">
+                        <th>Origen/Destino</th>
+                        <th>Fecha</th>
+                        <th>Hora</th>
+                        <th>Ruta</th>
+                        <th>Cancelar</th>
+                      </tr>
+                    </thead>
+                    <tbody>
+                      <tr
                         v-for="route in routesActive"
                         :key="route[0].request_id"
                       >
-                        <div class="card-header" id="headingOne3">
-                          <h2 class="mb-0">
-                            <button
-                              class="btn btn-link btn-block text-left"
-                              type="button"
-                              data-toggle="collapse"
-                              :data-target="`#data${route[0].request_id}`"
-                              aria-expanded="true"
-                              :aria-controls="`data${route[0].request_id}`"
-                              style="color: #06416d"
-                            >
-                              <div>
-                                Origen:
-                                {{ route[1][0].address.split(",")[0] }}
-                              </div>
-                              <div>
-                                Destino:
-                                {{ route[1][1].address.split(",")[0] }}
-                              </div>
-                            </button>
-                          </h2>
-                        </div>
-                        <div
-                          :id="`data${route[0].request_id}`"
-                          class="collapse"
-                          aria-labelledby="headingOne3"
-                          data-parent="#accordionExample"
-                        >
-                          <div class="card-body">
-                            <div>Dia: {{ route[0].date }}</div>
-                            <div>Hora: {{ route[0].time }}</div>
-
-                            <div class="row">
-                              <div class="col">
-                                <button
-                                  type="button"
-                                  class="btn btn-outline-dark btn-block button"
-                                  @click="deleteRoute(route[0].request_id)"
-                                  style="margin: 5% 0 5% 0"
-                                >
-                                  Cancelar Servicio
-                                </button>
-                              </div>
-                              <div class="col">
-                                <button
-                                  type="button"
-                                  class="btn btn-outline-dark btn-block button"
-                                  style="margin: 5% 0 5% 0"
-                                  @click="routePassengerItemPressed(route[1])"
-                                >
-                                  Ver Ruta
-                                </button>
-                              </div>
-                            </div>
-                          </div>
-                        </div>
-                      </div>
-                    </div>
-                  </div>
-                  <div
-                    class="tab-pane fade"
-                    id="profile"
-                    role="tabpanel"
-                    aria-labelledby="profile-tab"
-                  >
-                    <div class="accordion" id="accordionExample2">
-                      <div
-                        class="card"
-                        v-for="route1 in routesChoosed"
-                        :key="route1.id"
+                        <td>
+                          {{ route[1][0].address.split(",")[0] }}/{{
+                            route[1][1].address.split(",")[0]
+                          }}
+                        </td>
+                        <td>{{ route[0].date }}</td>
+                        <td>{{ route[0].time }}</td>
+                        <td>
+                          <img
+                            type="button"
+                            src="~@/assets/map.png"
+                            width="24"
+                            height="24"
+                            alt="map"
+                            @click="routePassengerItemPressed(route[1])"
+                          />
+                        </td>
+                        <td>
+                          <img
+                            type="button"
+                            src="~@/assets/remove.png"
+                            width="24"
+                            height="24"
+                            alt="remove"
+                            @click="deleteRoute(route[0].request_id)"
+                          />
+                        </td>
+                      </tr>
+                    </tbody>
+                  </table>
+                </div>
+                <div
+                  class="tab-pane fade"
+                  id="profile"
+                  role="tabpanel"
+                  aria-labelledby="profile-tab"
+                >
+                  <table class="table table-dark">
+                    <thead>
+                      <tr class="text-warning">
+                        <th>Origen/Destino</th>
+                        <th>Fecha</th>
+                        <th>Hora</th>
+                        <th>Ruta</th>
+                      </tr>
+                    </thead>
+                    <tbody>
+                      <tr
+                        v-for="route in routesChoosed"
+                        :key="route[0].request_id"
                       >
-                        <div class="card-header" id="headingOne">
-                          <h2 class="mb-0">
-                            <button
-                              class="btn btn-link btn-block text-left"
-                              type="button"
-                              data-toggle="collapse"
-                              :data-target="`#data${route1.id}`"
-                              aria-expanded="true"
-                              :aria-controls="`data${route1.id}`"
-                              style="color: #06416d"
-                            >
-                              <div>
-                                Origen:
-                                {{ route1.origin.address.split(",")[0] }}
-                              </div>
-                              <div>
-                                Destino:
-                                {{ route1.destination.address.split(",")[0] }}
-                              </div>
-                            </button>
-                          </h2>
-                        </div>
-                        <div
-                          :id="`data${route1.id}`"
-                          class="collapse"
-                          aria-labelledby="headingOne"
-                          data-parent="#accordionExample2"
-                        >
-                          <div class="card-body">
-                            <div>Dia: {{ route1.date }}</div>
-                            <div>Hora: {{ route1.time }}</div>
-                            <div>Valor: {{ route1.value }}</div>
-
-                            <div class="row">
-                              <div class="col">
-                                <button
-                                  type="button"
-                                  class="btn btn-outline-dark btn-block button"
-                                  style="margin: 5% 0 5% 0"
-                                  @click="routePassengerMade(route1)"
-                                >
-                                  Ver Ruta
-                                </button>
-                              </div>
-                            </div>
-                          </div>
-                        </div>
-                      </div>
-                    </div>
-                  </div>
-                  <div
-                    class="tab-pane fade"
-                    id="contact"
-                    role="tabpanel"
-                    aria-labelledby="contact-tab"
-                  >
-                    <div class="accordion" id="accordionExample3">
-                      <div
-                        class="card"
-                        v-for="route2 in routesMade"
-                        :key="route2.id"
+                        <td>
+                          {{ route[1][0].address.split(",")[0] }}/{{
+                            route[1][1].address.split(",")[0]
+                          }}
+                        </td>
+                        <td>{{ route[0].date }}</td>
+                        <td>{{ route[0].time }}</td>
+                        <td>
+                          <img
+                            type="button"
+                            src="~@/assets/map.png"
+                            width="24"
+                            height="24"
+                            alt="map"
+                            @click="routePassengerItemPressed(route[1])"
+                          />
+                        </td>
+                      </tr>
+                    </tbody>
+                  </table>
+                </div>
+                <div
+                  class="tab-pane fade"
+                  id="contact"
+                  role="tabpanel"
+                  aria-labelledby="contact-tab"
+                >
+                  <table class="table table-dark">
+                    <thead>
+                      <tr class="text-warning">
+                        <th>Origen/Destino</th>
+                        <th>Fecha</th>
+                        <th>Hora</th>
+                        <th>Ruta</th>
+                      </tr>
+                    </thead>
+                    <tbody>
+                      <tr
+                        v-for="route in routesMade"
+                        :key="route[0].request_id"
                       >
-                        <div class="card-header" id="headingOne2">
-                          <h2 class="mb-0">
-                            <button
-                              class="btn btn-link btn-block text-left"
-                              type="button"
-                              data-toggle="collapse"
-                              :data-target="`#data${route2.id}`"
-                              aria-expanded="true"
-                              :aria-controls="`data${route2.id}`"
-                              style="color: #06416d"
-                            >
-                              <div>
-                                Origen:
-                                {{ route2.origin.address.split(",")[0] }}
-                              </div>
-                              <div>
-                                Destino:
-                                {{ route2.destination.address.split(",")[0] }}
-                              </div>
-                            </button>
-                          </h2>
-                        </div>
-                        <div
-                          :id="`data${route2.id}`"
-                          class="collapse"
-                          aria-labelledby="headingOne2"
-                          data-parent="#accordionExample3"
-                        >
-                          <div class="card-body">
-                            <div>Dia: {{ route2.date }}</div>
-                            <div>Hora: {{ route2.time }}</div>
-                            <div>Value: {{ route2.value }}</div>
-
-                            <div class="row">
-                              <div class="col">
-                                <button
-                                  type="button"
-                                  class="btn btn-outline-dark btn-block button"
-                                  style="margin: 5% 0 5% 0"
-                                  @click="routePassengerMade(route2)"
-                                >
-                                  Ver Ruta
-                                </button>
-                              </div>
-                              <div class="col">
-                                <button
-                                  type="button"
-                                  class="btn btn-outline-dark btn-block button"
-                                  style="margin: 5% 0 5% 0"
-                                  @click="returnRoute(route2)"
-                                >
-                                  Calificar Pasajeros
-                                </button>
-                              </div>
-                            </div>
-                          </div>
-                        </div>
-                      </div>
-                    </div>
-                  </div>
+                        <td>
+                          {{ route[1][0].address.split(",")[0] }}/{{
+                            route[1][1].address.split(",")[0]
+                          }}
+                        </td>
+                        <td>{{ route[0].date }}</td>
+                        <td>{{ route[0].time }}</td>
+                        <td>
+                          <img
+                            type="button"
+                            src="~@/assets/map.png"
+                            width="24"
+                            height="24"
+                            alt="map"
+                            @click="routePassengerItemPressed(route[1])"
+                          />
+                        </td>
+                      </tr>
+                    </tbody>
+                  </table>
                 </div>
               </div>
             </div>
           </div>
         </div>
-        <div class="col-12 col-md-6 mt-md-4 mb-5">
+        <div class="col-12 col-lg-6 mb-3 mb-lg-0">
           <DirectionsMapView />
         </div>
       </div>
@@ -267,12 +195,12 @@
 </template>
 
 <script>
-import Header from "../components/Header";
+import Header from "../components/Header.vue";
 import FooterwithBackground from "../components/FooterwithBackground.vue";
 import DirectionsMapView from "../components/DirectionsMapView.vue";
 import { EventBus } from "@/EventBus.js";
 import firebase from "firebase";
-import RequestCo from "../controller/RequestController"
+import RequestCo from "../controller/RequestController";
 
 export default {
   name: "MyServicesDriver",
@@ -314,10 +242,10 @@ export default {
       this.$router.push("/passenger");
     },
     getRoutesActives() {
-      RequestCo.getRequests((data)=>{
-        this.routesActive = []
-        this.routesActive = data
-      })
+      RequestCo.getRequests((data) => {
+        this.routesActive = [];
+        this.routesActive = data;
+      });
     },
     getRoutesChoosed() {
       const db = firebase.firestore();
@@ -359,27 +287,27 @@ export default {
       });
     },
     deleteRoute(request) {
-        RequestCo.deleteRequests(request,(response)=>{
+      RequestCo.deleteRequests(request, (response) => {
         this.$bvToast.toast("Postulación Cancelada Correctamente!", {
-        title: "Postulación Cancelada",
-        autoHideDelay: 5000,
-        appendToast: true,
-        variant: "success",
-        solid: true,
+          title: "Postulación Cancelada",
+          autoHideDelay: 5000,
+          appendToast: true,
+          variant: "success",
+          solid: true,
+        });
+        this.routesActive = response;
       });
-      this.routesActive = response
-      })
     },
     returnRoute(route) {
       this.$router.push("/service-ended");
     },
     routePassengerItemPressed(route) {
-      this.request.origin.lat = Number(route[0].lat)
-      this.request.origin.lng = Number(route[0].lng)
-      this.request.origin.address = route[0].address
-      this.request.destination.lat = Number(route[1].lat)
-      this.request.destination.lng = Number(route[1].lng)
-      this.request.destination.address = route[1].address
+      this.request.origin.lat = Number(route[0].lat);
+      this.request.origin.lng = Number(route[0].lng);
+      this.request.origin.address = route[0].address;
+      this.request.destination.lat = Number(route[1].lat);
+      this.request.destination.lng = Number(route[1].lng);
+      this.request.destination.address = route[1].address;
       EventBus.$emit("passengerRoutes-data", [this.request]);
     },
     routePassengerMade(route) {
