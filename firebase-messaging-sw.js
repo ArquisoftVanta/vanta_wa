d2bc9d3b7e3565cb6d30ec1f15415cf13863c5f7
Julian <ojtinjacar@unal.edu.co>
2021-08-19 14:48:55 -0500

    Permitir Notificaciones

---
+++ firebase-messaging-sw.js
@@ -0,0 +1,28 @@
+importScripts('https://www.gstatic.com/firebasejs/8.6.1/firebase-app.js');
+importScripts('https://www.gstatic.com/firebasejs/8.6.1/firebase-messaging.js');
+
+var firebaseConfig = {
+    apiKey: "AIzaSyAPoQCAkJr6IqI9As8n9rq8lLtWObSl3yw",
+    authDomain: "vanta-132d2.firebaseapp.com",
+    projectId: "vanta-132d2",
+    storageBucket: "vanta-132d2.appspot.com",
+    messagingSenderId: "557344769302",
+    appId: "1:557344769302:web:7c3ff58d300d60fbb5fc37",
+    measurementId: "G-07FD4P162Z"
+};
+firebase.initializeApp(firebaseConfig);
+const messaging = firebase.messaging();
+
+
+messaging.onBackgroundMessage(function(payload) {
+    console.log('[firebase-messaging-sw.js] Received background message ', payload);
+    // Customize notification here
+    const notificationTitle = 'Background Message Title';
+    const notificationOptions = {
+        body: 'Background Message body.',
+        icon: 'http://localhost/notificaciones/img/icon.png'
+    };
+
+    self.registration.showNotification(notificationTitle,
+        notificationOptions);
+});
--- src/main.js
+++ src/main.js
@@ -6,10 +6,10 @@ import axios from "axios";
 import VueAxios from "vue-axios";
 import firebase from "firebase";
 import {
-  BootstrapVue,
-  IconsPlugin,
-  BVToastPlugin,
-  ToastPlugin,
+    BootstrapVue,
+    IconsPlugin,
+    BVToastPlugin,
+    ToastPlugin,
 } from "bootstrap-vue";
 import moment from "moment";
 
@@ -17,20 +17,56 @@ import "bootstrap/dist/css/bootstrap.css";
 import "bootstrap-vue/dist/bootstrap-vue.css";
 
 if (process.env.NODE_ENV != "production") {
-  process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
+    process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
 }
+
 var firebaseConfig = {
-  apiKey: "AIzaSyBYLj8XtE_HgnebkWa0Go4W_4bnsttl6nA",
-  authDomain: "wheelsus.firebaseapp.com",
-  databaseURL: "https://wheelsus.firebaseio.com",
-  projectId: "wheelsus",
-  storageBucket: "wheelsus.appspot.com",
-  messagingSenderId: "292187867274",
-  appId: "1:292187867274:web:59126bc71ef45f61a94573",
-  measurementId: "G-7B2LEW813H",
+    apiKey: "AIzaSyAPoQCAkJr6IqI9As8n9rq8lLtWObSl3yw",
+    authDomain: "vanta-132d2.firebaseapp.com",
+    projectId: "vanta-132d2",
+    storageBucket: "vanta-132d2.appspot.com",
+    messagingSenderId: "557344769302",
+    appId: "1:557344769302:web:7c3ff58d300d60fbb5fc37",
+    measurementId: "G-07FD4P162Z"
 };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
+firebase.analytics();
+const messaging = firebase.messaging();
+
+window.onload = function() {
+    messaging.requestPermission()
+        .then(function() {
+            if ("serviceWorker" in navigator) {
+                navigator.serviceWorker
+                    .register("../firebase-messaging-sw.js")
+                    .then(function(registration) {
+                        console.log("Registration successful, scope is:", registration.scope);
+                        messaging.getToken({
+                                vapidKey: 'BFpT_o73pzbP3KXOsqBp9kUB6ZKPbEmQPpiNRFqVzmVXWTL_TAqxBa4gHM63E7XMKWU7_-Vz16SIRxWXPayA4wg',
+                                serviceWorkerRegistration: registration
+                            })
+                            .then((currentToken) => {
+                                if (currentToken) {
+                                    console.log('current token for client: ', currentToken);
+                                } else {
+                                    console.log('No registration token available. Request permission to generate one.');
+                                }
+                            }).catch((err) => {
+                                console.log('An error occurred while retrieving token. ', err);
+                                // catch error while creating client token
+                            });
+                    })
+                    .catch(function(err) {
+                        console.log("Service worker registration failed, error:", err);
+                    });
+            }
+        }).catch(function(err) {
+            console.log('No se ha recibido el permiso');
+            showAlert();
+        });
+
+}
 
 Vue.config.productionTip = false;
 
@@ -39,10 +75,10 @@ Vue.use(IconsPlugin);
 Vue.use(BVToastPlugin);
 Vue.use(ToastPlugin);
 new Vue({
-  router,
-  VueAxios,
-  axios,
-  store,
-  moment,
-  render: (h) => h(App),
+    router,
+    VueAxios,
+    axios,
+    store,
+    moment,
+    render: (h) => h(App),
 }).$mount("#app");
